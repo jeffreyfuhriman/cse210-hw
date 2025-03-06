64f54a06da7e4ebc60d075a0a484761c4fc3b456
jeffreyfuhriman <jfuhriman@byui.edu>
2025-03-05 23:39:04 -0700

    Completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 5);
+        shapes.Add(s1);
+        Rectangle s2 = new Rectangle("Green", 5, 7);
+        shapes.Add(s2);
+        Circle s3 = new Circle("Blue", 3);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area:F2}.");
+        }
     }
 }
